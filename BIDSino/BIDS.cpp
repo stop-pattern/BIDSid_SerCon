8c1477810b8dd0b0df46507442f1a663c0c71393
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-02-25 03:04:19 +0900

    [add] add getter & setter

--- BIDSino/BIDS.cpp
+++ BIDSino/BIDS.cpp
@@ -2,12 +2,12 @@
 //Base : stop-pattern
 //v202 Support : TetsuOtter
 
-
-#include <arduino.h>
 #include "BIDS.h"
+#include <arduino.h>
 #define INITIAL_BAUDRATE 19200
 #define TARGET_VERSION 202
 
+/* ----- constructor ----- */
 c_BIDS::c_BIDS() {
   this->Version = VersionCheck(TARGET_VERSION, INITIAL_BAUDRATE);
 }
@@ -18,11 +18,11 @@ c_BIDS::c_BIDS(int v = TARGET_VERSION, int baudRate = INITIAL_BAUDRATE) {
   this->Version = VersionCheck(v, baudRate);
 }
 
-float c_BIDS::SerialGet(String Command) {
+/* ----- getData ----- */
+auto c_BIDS::SerialGet(String Command) {
   unsigned long sendtime;
   int cnt = 0;
-  Serial.print(Command);
-  Serial.print("\n");
+  Serial.println(Command);
   sendtime = millis();
   while (Serial.available() <= 0) {
     //failed to get data (after 1 minute)
@@ -41,22 +41,22 @@ float c_BIDS::SerialGet(String Command) {
   return (DataGet(retFlo, Command + "X") != "") ? retFlo : 0;
 }
 
-float c_BIDS::DataGet(String Identifier, String Symbol, int Num) {
+/* ----- DataGet ----- */
+auto c_BIDS::DataGet(String Identifier, String Symbol, int Num) {
   return SerialGet("TR" + Identifier + Symbol + String(Num));
 }
-float c_BIDS::DataGet(String Identifier, int Num) {
+auto c_BIDS::DataGet(String Identifier, int Num) {
   return DataGet(Identifier, "", Num);
 }
-String c_BIDS::DataGet(float& Data, String Header = "TR") {
-  if (Serial.available() <= 0)return -1;
+String c_BIDS::DataGet(float &Data, String Header = "TR") {
+  if (Serial.available() <= 0) return -1;
   String GotData = Serial.readStringUntil('\n');
   GotData.replace("\n", "");
 
   if (GotData.startsWith(Header)) {
     Data = GotData.substring(GotData.indexOf("X")).toFloat();
     return GotData;
-  }
-  else {
+  } else {
     Data = 0;
     return "";
   }
@@ -71,12 +71,115 @@ int c_BIDS::VersionCheck(int VersionNum, int BaudRate) {
       Serial.end();
       Serial.begin(BaudRate);
     }
-  } else vnum = DataGet("V", VersionNum);
+  } else
+    vnum = DataGet("V", VersionNum);
 
   ret = vnum < VersionNum ? VersionNum : vnum;
   return ret;
 }
 
+/* ----- setter ----- */
+// Train Operation Instruction
+int setReverser(int pos) {
+  if (-1 <= pos && pos <= 1)
+    return DataGet("R", pos);
+  return 0;
+};
+int setController(int hand) {
+  return DataGet("S", hand);
+};
+int setController(int power, int brake) {
+  if (brake >= 0) {
+    int p = DataGet("P", power);
+    int b = DataGet("B", brake);
+  }
+  if (p == 0 && b == 0)
+    return 0;
+};
+// keys
+int setBVEKeyPush(int key) {
+  return DataGet("K", "P", key);
+};
+int setBVEKeyRelease(int key) {
+  return DataGet("K", "R", key);
+};
+int setKeyboardDown(int) {
+  return DataGet("K", "D", key);
+};
+int setKeyboardUp(int) {
+  return DataGet("K", "U", key);
+};
+
+/* ----- getter ----- */
+// Vehicle Specification
+int getBrakeNotches() {
+  return DataGet("I", "C", Car::BrakeNotchCount);
+};
+int getPowerNotches() {
+  return DataGet("I", "C", Car::PowerNotchCount);
+};
+int getAtsNotch() {
+  return DataGet("I", "C", Car::ATSNotchCount);
+};
+int getB67Notch() {
+  return DataGet("I", "C", Car::B67NotchCount);
+};
+int getCarNumber() {
+  return DataGet("I", "C", Car::CarNumber);
+};
+// State Quantity of Vehicle
+double getLocation() {
+  return DataGet("I", "E", E::Location);
+};
+float getSpeed() {
+  return DataGet("I", "E", E::Speed);
+};
+int getTime() {
+  return DataGet("I", "E", E::Time);
+};
+float getBcPressure() {
+  return DataGet("I", "E", E::BcPressure);
+};
+float getMrPressure() {
+  return DataGet("I", "E", E::MrPressure);
+};
+float getErPressure() {
+  return DataGet("I", "E", E::ErPressure);
+};
+float getBpPressure() {
+  return DataGet("I", "E", E::BpPressure);
+};
+float getSapPressure() {
+  return DataGet("I", "E", E::SapPressure);
+};
+float getCurrent() {
+  return DataGet("I", "E", E::Current);
+};
+// Train Operation Instruction
+int getBrake() {
+  return DataGet("I", "H", Handle::BrakeNotch);
+};
+int getPower() {
+  return DataGet("I", "H", Handle::PowerNotch;
+};
+int getReverser() {
+  return DataGet("I", "H", Handle::Reverser;
+};
+int getConstantSpeed() {
+  return DataGet("I", "H", Handle::ConstantSpeed;
+};
+// Panel
+int getPanel(int index) {
+  return DataGet("I", "P", index);
+};
+// Sound
+int getSound(int index) {
+  return DataGet("I", "S", index);
+};
+// Door
+int getDoor() {
+  return DataGet("I", "D", 0);
+};
 
 enum c_BIDS::Reverser : int {
   Rear = -1,    //後
@@ -84,26 +187,26 @@ enum c_BIDS::Reverser : int {
   Front = 1,    //前
 };
 enum c_BIDS::key : int {
-  Horn_1,               //default:enter
-  Horn_2,               //default:+
-  MusicHorn,            //default:-
-  ConstantSpeedControl, //default:BackSpace
-  S,                    //default:Space
-  A_1,                  //default:Insert
-  A_2,                  //default:Delete
-  B_1,                  //default:Home
-  B_2,                  //default:End
-  C_1,                  //default:PageUp
-  C_2,                  //default:Next
-  D,                    //default:2
-  E,                    //default:3
-  F,                    //default:4
-  G,                    //default:5
-  H,                    //default:6
-  I,                    //default:7
-  J,                    //default:8
-  K,                    //default:9
-  L,                    //default:0
+  Horn_1,                //default:enter
+  Horn_2,                //default:+
+  MusicHorn,             //default:-
+  ConstantSpeedControl,  //default:BackSpace
+  S,                     //default:Space
+  A_1,                   //default:Insert
+  A_2,                   //default:Delete
+  B_1,                   //default:Home
+  B_2,                   //default:End
+  C_1,                   //default:PageUp
+  C_2,                   //default:Next
+  D,                     //default:2
+  E,                     //default:3
+  F,                     //default:4
+  G,                     //default:5
+  H,                     //default:6
+  I,                     //default:7
+  J,                     //default:8
+  K,                     //default:9
+  L,                     //default:0
 };
 enum c_BIDS::Car : int {
   BrakeNotchCount,  //Number of Brake Notches
@@ -129,3 +232,19 @@ enum c_BIDS::Handle : int {
   Reverser,       //Reverser Position
   ConstantSpeed,  //Constant Speed Control
 };
+/*
+enum c_BIDS::Error : String {
+  0 = "原因不明エラー",
+  1 = "コンバータとBIDSpp.dllとの間の接続が確立されていない",
+  2 = "要求情報コードの数値部が不正",
+  3 = "要求情報コードの記号部が不正",
+  4 = "識別子が不正",
+  5 = "数値変換がオーバーフローした",
+  6 = "要求情報コードの数値部に数値以外が混入している",
+  7 = "要求情報コードの数値部もしくは記号部が不正",
+  8 = "BVEのウィンドウハンドルを取得できない(キーイベント送信時)",
+  9 = "(情報なし)",
+  10 = "(情報なし)",
+  11 = "(情報なし)",
+};
+*/
--- BIDSino/BIDS.h
+++ BIDSino/BIDS.h
@@ -1,33 +1,77 @@
-//BIDS.h
-//Base : stop-pattern
-//v202 Support : TetsuOtter
-
+// BIDS.h
+// Base : stop-pattern
+// v202 Support : TetsuOtter
 
 #ifndef _BIDS_
 #define _BIDS_
 
 class c_BIDS {
-  public:
-    c_BIDS();
-    c_BIDS(int);
-    c_BIDS(int, int);
+ public:
+  /* ----- constructor ----- */
+  c_BIDS();
+  c_BIDS(int);
+  c_BIDS(int, int);
 
-    //int DataGet(byte*, byte*, int);
-    float DataGet(String, String, int);
-    float DataGet(String, int);
-    String DataGet(float&, String);
+  /* ----- setter ----- */
+  // Train Operation Instruction
+  int setReverser(int);
+  int setController(int);
+  int setController(int, int);
+  // keys
+  int setBVEKeyPush(int);
+  int setBVEKeyRelease(int);
+  int setKeyboardDown(int);
+  int setKeyboardUp(int);
 
-    int Version;
+  /* ----- getter ----- */
+  // Vehicle Specification
+  int getBrakeNotches();
+  int getPowerNotches();
+  int getAtsNotch();
+  int getB67Notch();
+  int getCarNumber();
+  // State Quantity of Vehicle
+  double getLocation();
+  float getSpeed();
+  int getTime();
+  float getBcPressure();
+  float getMrPressure();
+  float getErPressure();
+  float getBpPressure();
+  float getSapPressure();
+  float getCurrent();
+  // Train Operation Instruction
+  int getBrake();
+  int getPower();
+  int getReverser();
+  int getConstantSpeed();
+  // Panel
+  int getPanel(int);
+  // Sound
+  int getSound(int);
+  // Door
+  int getDoor();
 
-    enum Reverser : int;    //for TRR
-      enum key : int;       //for TRK
-      enum Car : int;       //for TRIC
-      enum E : int;         //for TRIE (tentative)
-      enum Handle : int;    //for TRIH
+  /* ----- definition ----- */
+  enum Reverser : int;  // for TRR
+  enum key : int;       // for TRK
+  enum Car : int;       // for TRIC
+  enum E : int;         // for TRIE (tentative)
+  enum Handle : int;    // for TRIH
+                        //enum Error : int;     // for TRE
 
-    private:
-      float SerialGet(String);
-      int VersionCheck(int, int);
-  };
+ private:
+  /* ----- getData ----- */
+  auto SerialGet(String);
 
-#endif //_BIDS_
+  /* ----- DataGet ----- */
+  auto DataGet(String, String, int);
+  auto DataGet(String, int);
+  String DataGet(double &, String);
+
+  /* ----- getData ----- */
+  int Version;
+  int VersionCheck(int, int);
+};
+
+#endif  //_BIDS_
